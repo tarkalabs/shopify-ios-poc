ef3350899cc57237b99cf902790fc6823818bedc
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2015-10-14 11:50:15 +0530

    Initial Commit

---
+++ shopify-ios-pocTests/shopify_ios_pocTests.swift
@@ -0,0 +1,36 @@
+//
+//  shopify_ios_pocTests.swift
+//  shopify-ios-pocTests
+//
+//  Created by Vagmi on 14/10/15.
+//  Copyright © 2015 Vagmi. All rights reserved.
+//
+
+import XCTest
+@testable import shopify_ios_poc
+
+class shopify_ios_pocTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
